14ff2900f8f8d86ad1b565468b4aad0c0d5afc52
"Anchit Jain" <"anchitjain1234@gmail.com">
2021-12-26 22:50:12 +0530

    Add remove liquidity functionality

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -19,8 +19,9 @@ contract Factory {
 
     uint256 public numPairs;
 
-    function addLiquidity(address _tokenA, address _tokenB, uint256 _amountOfADesired, uint256 _amountOfBDesired) 
+    function addLiquidity(address _token0, address _token1, uint256 _amountOfADesired, uint256 _amountOfBDesired) 
         public returns(uint256 amountA, uint256 amountB) {
+        (address _tokenA, address _tokenB) = sortTokens(_token0, _token1);
         
         //validate if pair exists
         if (getPair[_tokenA][_tokenB] == address(0)) {
@@ -31,24 +32,49 @@ contract Factory {
             (amountA, amountB) = (_amountOfADesired, _amountOfBDesired);
         } else {
             (uint256 reserveA, uint256 reserveB) = getReserves(_tokenA, _tokenB);
-            // pair already exists, quote the amount to be put
-            uint256 amountOfBOptimal = quote(_amountOfADesired, reserveA, reserveB);
 
-            if (amountOfBOptimal <= _amountOfBDesired) {
-                (amountA, amountB) = (_amountOfADesired, amountOfBOptimal);
+            if (reserveA == 0 && reserveB == 0) {
+                // if the pool is empty now consider this as the first deposit
+                (amountA, amountB) = (_amountOfADesired, _amountOfBDesired);
             } else {
-                uint256 amountOfAOptimal = quote(_amountOfBDesired, reserveB, reserveA);
-                (amountA, amountB) = (amountOfAOptimal, _amountOfBDesired);
+                // pair already exists, quote the amount to be put
+                uint256 amountOfBOptimal = quote(_amountOfADesired, reserveA, reserveB);
+
+                if (amountOfBOptimal <= _amountOfBDesired) {
+                    (amountA, amountB) = (_amountOfADesired, amountOfBOptimal);
+                } else {
+                    uint256 amountOfAOptimal = quote(_amountOfBDesired, reserveB, reserveA);
+                    (amountA, amountB) = (amountOfAOptimal, _amountOfBDesired);
+                }
             }
         }
+        console.log("amountA %s, amountB %s", amountA, amountB);
 
         //transfer tokens to the pair
         address pair = getPair[_tokenA][_tokenB];
+
+        IERC20(_tokenA).approve(address(this), amountA);
+        IERC20(_tokenB).approve(address(this), amountB);
+
         IERC20(_tokenA).transferFrom(msg.sender, pair, amountA);
         IERC20(_tokenB).transferFrom(msg.sender, pair, amountB);
+        console.log("transferred");
 
         //update reserves
         Pair(pair).updateReserves();
+        console.log("reserve updated");
+
+        //Provide LP tokens to the user. For simplicity sum of both
+        Pair(pair).mint(msg.sender, amountA.add(amountB));
+    }
+
+    function removeLiquidity(address _token0, address _token1, uint256 _liquidityTokens) public {
+        (address _tokenA, address _tokenB) = sortTokens(_token0, _token1);
+        require (getPair[_tokenA][_tokenB] != address(0), "Factory: Invalid Pair"); 
+        
+        address pair = getPair[_tokenA][_tokenB];
+        require(IERC20(pair).balanceOf(msg.sender) >= _liquidityTokens, "Factory: Insufficient balance present");
+        Pair(pair).burn(msg.sender, _liquidityTokens);
     }
 
     function tradeBForA(uint256 _amountOfTokenA, uint256 _minTokensBToGet, address _tokenA, address _tokenB) public {
@@ -65,11 +91,18 @@ contract Factory {
         Pair(pair).swap(uint256(0), amountOfBOut, msg.sender);
     } 
 
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, "IDENTICAL_ADDRESSES");
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), "ZERO_ADDRESS");
+    }
+
     function createPair(address _tokenA, address _tokenB) internal returns(address pair) {
         require(_tokenA != _tokenB, "Factory: Token addresses are equal");
 
         pair = address(new Pair(_tokenA, _tokenB));
         getPair[_tokenA][_tokenB] = pair;
+        getPair[_tokenB][_tokenA] = pair;
         numPairs++;
         allPairs.push(pair);
     }
--- contracts/Pair.sol
+++ contracts/Pair.sol
@@ -10,7 +10,10 @@ pragma solidity ^0.8.7;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-contract Pair {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "./LPToken.sol";
+
+contract Pair is ERC20 {
     using SafeMath for uint256;
 
     address public factory;
@@ -20,7 +23,9 @@ contract Pair {
     uint256 private reserveTokenA;
     uint256 private reserveTokenB;
 
-    constructor(address _tokenA, address _tokenB) {
+    LPToken lpToken;
+
+    constructor(address _tokenA, address _tokenB) ERC20("LPToken", "LPT") {
         tokenA = _tokenA;
         tokenB = _tokenB;
         factory = msg.sender;
@@ -34,6 +39,28 @@ contract Pair {
     function updateReserves() public onlyFactory {
         reserveTokenA = IERC20(tokenA).balanceOf(address(this));
         reserveTokenB = IERC20(tokenB).balanceOf(address(this));
+    }
+
+    function mint(address account, uint256 amount) public onlyFactory {
+        _mint(account, amount);
+        updateReserves();
+    }
+
+    function burn(address _to, uint256 _liquidity) public onlyFactory {
+        uint256 totalSupply = reserveTokenA.add(reserveTokenB);
+        console.log("totalSupply %s", totalSupply);
+        require(_liquidity <= totalSupply, "Pair: Invalid amount burned");
+
+        uint256 amountAToTransfer = _liquidity.mul(reserveTokenA).div(totalSupply);
+        uint256 amountBToTransfer = _liquidity.mul(reserveTokenB).div(totalSupply);
+
+        IERC20(tokenA).approve(address(this), amountAToTransfer);
+        IERC20(tokenB).approve(address(this), amountBToTransfer);
+
+        IERC20(tokenA).transferFrom(address(this), _to, amountAToTransfer);
+        IERC20(tokenB).transferFrom(address(this), _to, amountBToTransfer);
+        _burn(_to, _liquidity);
+        updateReserves();
     }
 
     function swap(uint256 _amountTokenAOut, uint256 _amountTokenBOut, address _to) public {
@@ -53,8 +80,7 @@ contract Pair {
         //verify product constant formula, >= because the product won't be exactly equal due to precision loss
         require(balanceTokenA.mul(balanceTokenB) >= reserveTokenA.mul(reserveTokenB), "Product constant failed");
 
-        reserveTokenA = balanceTokenA;
-        reserveTokenB = balanceTokenB;
+        updateReserves();
     }
 
     function getProductConstant() public view returns(uint256) {
